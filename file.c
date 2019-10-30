300d494ec86e0c90fe345d89d99cb7b0afc6c001
Parva16TS <57166541+Parva16TS@users.noreply.github.com>
2019-10-30 21:11:02 +0530

    Update file.c

--- file.c
+++ file.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 int main()
 {
-    int i, n=8, t1 = 0, t2 = 1, m;
+    int i, n=8, t1 = 0, t2 = 1;
     printf("Fibonacci Series: ");
     for (i = 1; i <= n; ++i)
     {
         printf("%d  ", t1);
+        int m;
         m= t1 + t2;
         t1 = t2;
         t2 = m;
